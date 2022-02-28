91323ddbb69039ab72eee28058a06c3dc911c2d4
Alex <belohvostov.belohvostov@gmail.com>
2022-02-28 20:38:19 +0300

    login logout refresh controolers and services

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const router = require('./router/index.js')
+const errorMiddleware = require('./middlewares/error-middleware.js')
 
 const app = express()
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 app.use('/api', router)
+app.use(errorMiddleware)
 
 async function start() {
     try {
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,12 +4,14 @@ const bcrypt = require('bcrypt')
 const mailService = require('./mail-service')
 const tokenService = require('../service/token-service.js')
 const UserDto = require('../dtos/user-dto.js')
+const ApiError = require('../exceptions/api-error.js')
+const { refresh } = require('../controllers/user-controller.js')
 
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({email})
         if(candidate) {
-            throw new Error('User with dat email was created')
+            throw ApiError.BadRequest('User with dat email was created')
         }
 
         const hashPassword = await bcrypt.hash(password,3)
@@ -31,10 +33,55 @@ class UserService {
     async activate(activationLink) {
         const user = await UserModel.findOne({activationLink})
         if(!user) {
-            throw new Error('ActivationLink is not correct')
+            throw ApiError.BadRequest('ActivationLink is not correct')
         }
         user.isActivated = true
         await user.save()
+    }
+    
+    async login(email, password) {
+        const user = await UserModel.findOne({email})
+        if(!user) {
+            throw new ApiError.BadRequest('User with dat email was not found')
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password)
+        if(!isPassEquals) {
+            throw ApiError.BadRequest('Password is not correct')
+        }
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({...userDto})
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+        return {...tokens, user: userDto}
+    }
+
+    async logout(refreshToken) {
+        const token = await tokenService.removeToken(refreshToken)
+        return token
+    }
+    async refresh(refreshToken) {
+        if(!refreshToken) {
+            throw ApiError.UnAuthorizedError()
+        }
+        const userData = tokenService.validateRefreshToken(refreshToken)
+        const tokenFromDb = await tokenService.findToken(refreshToken)
+        if(!userData || tokenFromDb) {
+            throw ApiError.UnAuthorizedError()
+        }
+
+        const user = await UserModel.findById(userData.id)
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({...userDto})
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+        return {...tokens, user: userDto}
+    }
+
+    async getAllUsers() {
+        const users = await UserModel.find()
+        return users
     }
 }
 
