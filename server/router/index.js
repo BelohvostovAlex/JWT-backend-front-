246666f7125d29be6ce74392366952e209cfef08
BelohvostovAlex <belohvostov.belohvostov@gmail.com>
2022-03-01 14:14:52 +0300

    finished backend part

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router
 const userController = require('../controllers/user-controller.js')
 const {body} = require('express-validator')
+const authMiddleWare = require('../middlewares/auth-middleware.js')
 
 const router = new Router()
 
@@ -12,6 +13,6 @@ router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/activate/:link', userController.activate)
 router.get('/refresh', userController.refresh)
-router.get('/users', userController.getUsers)
+router.get('/users', authMiddleWare, userController.getUsers)
 
 module.exports = router
